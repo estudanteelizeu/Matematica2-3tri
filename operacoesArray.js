8d12bbd4d0cfa5a382d855673ce8e8d8419417d2
estudanteelizeu <elizeu.junior28@escola.pr.gov.br>
2026-09-18 11:30:31 -0300

    Create operacoesArray.js

---
+++ operacoesArray.js
@@ -0,0 +1,14 @@
+let frutas = ["abacaxi" , "manga" , "pera"];
+console.log(frutas);
+
+frutas.push("uva");
+console.log (frutas);
+
+frutas.unshift("morango");
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
+
+frutas.shift();
+console.log();
